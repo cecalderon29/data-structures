1caa43633343603698efea2bc63356b1e1679958
cecalderon29 <millacalderon298@gmail.com>
2025-08-25 13:16:57 -0500

    day 1 LL demo

--- Chapter 15 Class Notes/src/ListDemo.java
+++ Chapter 15 Class Notes/src/ListDemo.java
@@ -8,7 +8,31 @@ import java.util.ListIterator;
 public class ListDemo
 {
     public static void main(String[] args)
-    {
+    {   
+        /*The addLast method can be used to populate the list */
+        LinkedList <String> staff = new LinkedList<>();
+        staff.addLast("Tony");
+        staff.addLast("Natasha");
+        staff.addLast("Peter");
+        staff.addLast("Steve");
+        
+        System.out.println(staff);
+        
+        //The list is currently [Tony, Natasha, Peter, Steve]
+
+        /*
+         * The listIterator method creates a new ListIterator.
+         * that is positioned at the head of the list.
+         * The | is used to represent the iterator position.
+         */
+         ListIterator<String> iter = staff.listIterator();//    |Tony, Natasha, Peter, Steve
+
+        /* The next method advances the iterator OVER the  next element in the list */
+        iter.next(); //    Tony|Natasha, Peter, Steve
+
+        /*The next method also returns the element that the iterator passes over */
+        String avenger = iter.next(); //    TonyNatasha|Peter, Steve
+        System.out.println(avenger);//    Natasha
         
     }
 }
--- Chapter 15 Class Notes/src/StackDemo.java
+++ Chapter 15 Class Notes/src/StackDemo.java
@@ -4,10 +4,11 @@ import java.util.Stack;
  * This program simulates an undo stack. Note that operations
  * must be undone in the opposite order in which they are first
  * issued.
-*/
+*/ 
 public class StackDemo
 {
     public static void main(String[] args)
     {
+        System.out.println("It works!");
     }
 }
