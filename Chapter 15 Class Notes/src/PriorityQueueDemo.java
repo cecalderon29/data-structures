8a7703bdb7159f93779474f64c9a29725566e257
cecalderon29 <178575965+cecalderon29@users.noreply.github.com>
2025-09-18 13:23:47 -0500

    AQAWERTYUIOP[]

--- Chapter 15 Class Notes/src/PriorityQueueDemo.java
+++ Chapter 15 Class Notes/src/PriorityQueueDemo.java
@@ -10,5 +10,36 @@ public class PriorityQueueDemo
 {
     public static void main(String[] args)
     {
+        //Create a priority que of strings
+        //A priority needs tp be composed of Comparable objects
+        Queue<String> students = new PriorityQueue<>();
+        students.add("Neel");
+        students.add("Jonathan");
+        students.add("Cam");
+        students.add("Kaitlyn");
+        students.add("Dylan");
+
+        while (students.size() > 0)
+            System.out.println(students.remove());
+
+
+        //Create todo
+        //The workourder class has priotrity and description
+        Queue<WorkOrder> toDo = new PriorityQueue<>();
+        //Lower priority items are more important
+
+        toDo.add(new WorkOrder(3, "Water Plants"));
+        toDo.add(new WorkOrder(2, "Make Dinner"));
+        toDo.add(new WorkOrder(1, "Conquer World"));
+        toDo.add(new WorkOrder(9, "Play Videogames"));
+        toDo.add(new WorkOrder(1, "Study fpor the test"));
+
+        //Objects not stored in priority order
+        System.out.println(toDo);
+
+        //But they are removed in priority order
+        while (toDo.size() > 0)
+            System.out.println(toDo.remove());
     }
+
 }
--- Chapter 15 Class Notes/src/QueueDemo.java
+++ Chapter 15 Class Notes/src/QueueDemo.java
@@ -9,5 +9,31 @@ public class QueueDemo
 {
     public static void main(String[] args)
     {
+        //Create a print queue of stirngs using a linked list
+        Queue<String> jobs = new LinkedList<>();
+
+        //Add print jobs
+        jobs.add("Harry: Quarter 2 Expense Report");
+        jobs.add("Connor: Recipe for Banana Bread");
+        jobs.add("Katherine: Top Secret Document");
+
+        System.out.println("Printing:"+jobs.remove());
+
+        //Add some more jobs
+        jobs.add("Vardahn: Grocery List");
+        jobs.add("Katherine: Really Top Secret Document");
+        jobs.add("Katherine: Can I Get Fired For This?");
+
+        System.out.println("Printing:"+jobs.remove());
+        System.out.println("Printing:"+jobs.remove());
+
+
+        jobs.add("Boss: Katherine Termination Letter");
+
+        //Print rest of jobs
+        while(jobs.isEmpty() == false)
+        {
+            System.out.println("Printing:"+jobs.remove());
+        }
     }
 }
